c7204c6d627ef9ae9bd7914604d4b29ce86d9f0b
KabalSingh <93602155+KabalSingh@users.noreply.github.com>
2024-12-30 14:52:00 +0000

    new file

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+//let score="33"          => 33
+//let score="33abc"        =>NaN
+//let score=null;
+// let score=undefined;
+// let score=true;        true=1,false=0
+let score="hello"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+
+//console.log(valueInNumber);    //NaN =not a number
+
+
+// let isLoggedIn = 1;             1=>true,0=>false
+// let isLoggedIn = "";             ""=>false
+let isLoggedIn = "hello";            //"hello"=>false
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+
+let somenumber=33
+
+let stringNumber= String(somenumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
