ad7aaea65c06a06837cf63d2eb6391a64c8bc1b4
KabalSingh <93602155+KabalSingh@users.noreply.github.com>
2024-12-31 14:49:13 +0000

    test

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -46,4 +46,21 @@ let str1="Hello"
 let str2="World"
 
 let str3=str1+str2
-console.log(str3);
+//console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+3);
+// console.log(1+2+"3");
+
+// console.log(+true);
+// console.log(true+);
+
+let gameCounter=100
+
+// gameCounter++;
+// console.log(gameCounter);
+++gameCounter;
+// console.log(gameCounter);
+
+
