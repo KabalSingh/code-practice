f53d2f9e4048153686f3b0e5f6b0df2148779a5d
KabalSingh <93602155+KabalSingh@users.noreply.github.com>
2025-01-03 23:54:21 +0000

    test

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,37 @@
+const score = 400
+// console.log(score);
+
+
+const balance =new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber =123.47454
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds=100000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//*****************************  Maths  ******************************** */
+
+console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.611));
+// console.log(Math.ceil(4.11));
+// console.log(Math.floor(4.45611));
+// console.log(Math.min(5,9,3,96,23,6,9,6,3,3,2,2,9,5,5,52,));
+// console.log(Math.max(5,9,3,96,23,6,9,6,3,3,2,2,9,5,5,52,));
+
+
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log((Math.random()*10)+1);
+
+const max = 20
+const min = 10
+
+console.log(Math.floor(Math.random()*(max-min+1)+min));
