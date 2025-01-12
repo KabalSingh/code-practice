e7617e5c69dd1abfb7f364f4a9e4c68fd33d7085
KabalSingh <93602155+KabalSingh@users.noreply.github.com>
2025-01-12 22:46:46 +0000

    test

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -22,3 +22,35 @@ const user={
 // }
 
 // chai()
+
+// const chai=function(){
+//     let username="hello"
+//     console.log(this.username);
+// }
+
+// chai()
+
+const chai= () => {
+    let username="hello"
+    console.log(this);
+}
+
+// chai()
+
+
+
+
+// const addtwo=(num1, num2) => {
+//     return num1+num2
+// }
+
+// console.log(addtwo(4,8));
+
+
+
+
+// const addtwo=(num1, num2) => num1+num2
+// const addtwo=(num1, num2) => (num1+num2)
+// const addtwo=(num1, num2) => {username: "hello"}   //not used like it
+const addtwo=(num1, num2) => ({username: "hello"})
+console.log(addtwo(4,8));
