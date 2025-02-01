a3bf593671db80510db1109828fe8b46824cd47e
KabalSingh <93602155+KabalSingh@users.noreply.github.com>
2025-02-01 07:14:50 +0000

    test

---
+++ 07_projects/04-GuessTheNumber/script.js
@@ -0,0 +1,39 @@
+const randomNo=parseInt(Math.random()*100+1)
+
+const submit= document.querySelector('#subt')
+const userInput = document.querySelector('#guessField')
+const guessSlot= document.querySelector('.gueses')
+const remianing= document.querySelector('.lastResult')
+const lowOrHi= document.querySelector('.lowOrHi')
+const startOver = document.querySelector('.startOver')
+
+const p = document.createElement('p')
+
+let prevGuess = []
+let numGuess = 1
+
+let playGame = true
+
+function validGuess(guess) {
+    
+}
+
+function chcekGuess(guess) {
+    
+}
+
+function chcekGuess(guess) {
+    
+}
+
+function displayMessage(message) {
+    
+}
+
+function newGame() {
+    
+}
+
+function endGame() {
+    
+}
