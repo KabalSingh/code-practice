5d3b15a70909f259fc52c99f753e112729166882
KabalSingh <93602155+KabalSingh@users.noreply.github.com>
2025-02-14 00:20:23 +0000

    test

--- 09_advance_one/02_promises.js
+++ 09_advance_one/02_promises.js
@@ -54,3 +54,57 @@ promiseFour.then((user)=>{
     console.log(error);
 }).finally(() => console.log("The promise is either resolved or rejected")
 )
+
+
+const promiseFive=new Promise(function (resolve,reject) {
+    setTimeout(function () {
+        let error=true
+        // let error=false
+        if (!error) {
+            resolve({username:"JavaScript",password:"123"})
+        }else{
+            reject('ERROR :JS went wrong')
+        }
+    },1000)
+});
+
+
+async function consumePromiseFive(){
+    // const response=await promiseFive
+    // console.log(response);
+    try {
+        const response=await promiseFive
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+
+consumePromiseFive()
+
+// async function getAllUsers() {
+//     try {
+//         const response=await fetch('https://jsonplaceholder.typicode.com/users')
+//         // const data=response.json()
+//         // console.log(data);
+
+//         const data=await response.json()
+//         console.log(data);
+        
+//     } catch (error) {
+//         console.log("Error: ",error);
+        
+//     }
+// }
+
+// getAllUsers()
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response) =>{
+    return response.json()
+})
+.then((data) =>{
+    console.log(data);
+})
+.catch((error) =>console.log(error))
