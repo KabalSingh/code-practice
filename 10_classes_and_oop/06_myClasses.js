71498b5f0b6f286dd48d09be69419899ad5aedfa
KabalSingh <93602155+KabalSingh@users.noreply.github.com>
2025-02-19 02:10:06 +0000

    test

---
+++ 10_classes_and_oop/06_myClasses.js
@@ -0,0 +1,44 @@
+//ES6
+
+class User{
+    constructor(username, email, password){
+        this.username=username
+        this.email=email
+        this.password=password
+    }
+
+    encryptPassword(){
+        return `${this.password}abc`
+    }
+
+    changeUserName(){
+        return `${this.username.toUpperCase()}`
+    }
+}
+
+const chai =new User("chai","chai@gmail.com","123")
+
+// console.log(chai.encryptPassword());
+// console.log(chai.changeUserName());
+
+//behind the scene
+
+function user(username, email, password) {
+    this.username=username
+    this.email=email
+    this.password=password
+}
+
+user.prototype.encryptPassword=function () {
+    return `${this.password}abc`
+}
+
+user.prototype.encryptPassword=function () {
+    return `${this.username.toUpperCase()}`
+}
+
+const tea =new User("tea","tea@gmail.com","123")
+
+
+console.log(tea.encryptPassword());
+console.log(tea.changeUserName());
